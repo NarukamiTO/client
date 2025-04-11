1847d4bf77ed41fb651f7eb6f42a8e9d6f96a2da
Daniil Pryima <swimmin2@gmail.com>
2025-04-11 13:44:02 +0300

    initial: Add Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "narukami-client"
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    maven("https://central.sonatype.com/repository/maven-snapshots/") {
+      name = "Central Portal Snapshots"
+      content {
+        includeModule("dev.assasans.actionscript", "dev.assasans.actionscript.gradle.plugin")
+        includeModule("dev.assasans.actionscript", "gradle-actionscript-plugin")
+      }
+    }
+  }
+}
